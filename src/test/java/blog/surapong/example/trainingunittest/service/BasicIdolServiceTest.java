c3790c319dac011b9043d4be119894dd4b007cee
surapong <surapong.taotiamton@gmail.com>
2019-07-28 09:45:10 +0700

    Add new testcase

---
+++ src/test/java/blog/surapong/example/trainingunittest/service/BasicIdolServiceTest.java
@@ -0,0 +1,39 @@
+package blog.surapong.example.trainingunittest.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicIdolServiceTest {
+
+    @Test
+    public void getIdol_SuccessCase_ReturnCorrect() throws Exception {
+        ///////////////
+        // ARRANGE
+        ///////////////
+
+        ///////////////
+        // ACT
+        ///////////////
+
+        ///////////////
+        // ASSERT
+        //////////////
+    }
+
+    @Test
+    public void getIdol_NotFoundCase_ReturnNull() throws Exception {
+        ///////////////
+        // ARRANGE
+        ///////////////
+
+        ///////////////
+        // ACT
+        ///////////////
+
+        ///////////////
+        // ASSERT
+        //////////////
+    }
+
+}
